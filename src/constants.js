0928a7fe586859a9941288b1d9664e9e07bfdced
theopendle <theopendle@gmail.com>
2021-10-30 16:42:03 +0200

    feat: add default values to input

--- src/constants.js
+++ src/constants.js
@@ -6,13 +6,15 @@ module.exports = {
             name: "dev-server",
             type: "input",
             message: "Type the base URL of your development server",
-            validate: (input) => /./.test(input.match(input)) || "You shouldn't leave this field empty!"
+            validate: (input) => /./.test(input.match(input)) || "You shouldn't leave this field empty!",
+            default: "https://adobecampaign.com"
         },
         {
             name: "instances",
             type: "input",
             message: "Provide a comma-separated list of instance names. eg: 'mta,mc,interaction'",
-            validate: (input) => /[\w-]+,*/.test(input.match(input)) || "Please use only alphanumerics and '-' and '_'"
+            validate: (input) => /[\w-]+,*/.test(input.match(input)) || "Please use only alphanumerics and '-' and '_'",
+            default: "mta,mc"
         }
     ],
     JSON_COLORS: {
